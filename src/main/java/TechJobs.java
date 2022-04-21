7f29f8783b74dcef9fbee27ded4a4748c1492487
Joe Bao <toj7278@yahoo.com>
2022-04-21 01:00:50 -0500

    finished assignment 1 but tests aren't passing

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -119,7 +116,20 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+//        for (int i=0; i< someJobs.size(); i++) {
+        if (!someJobs.isEmpty()) {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.print("\n");
+                System.out.println("*****");
+//            HashMap job = someJobs.get(i);
+//            Set<Map.Entry<String,String>> s = job.entrySet();
+                for (Map.Entry<String, String> entry : job.entrySet()) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
+                System.out.println("*****");
+            }
+        } else {
+                System.out.print("No Results");
+        }
     }
 }
